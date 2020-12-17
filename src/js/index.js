09adcb7f1e614601730d3a65ead01f6ba8f53e5b
arronrees <64908445+arronrees@users.noreply.github.com>
2020-12-17 18:37:06 +0000

    Work page layout complete

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import '../styles/reset.scss';
 import '../styles/style.scss';
 import '../styles/home.scss';
 import '../styles/header.scss';
+import '../styles/workCards.scss';
+import '../styles/work.scss';
 
 import { navSlide } from './other/navSlide';
 
