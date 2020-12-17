653d455573c89c60f019e83cda0d70cbd5009c6f
arronrees <64908445+arronrees@users.noreply.github.com>
2020-12-17 17:13:27 +0000

    Header done mobile first

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 import '../styles/reset.scss';
 import '../styles/style.scss';
 import '../styles/home.scss';
-// import '../styles/header.scss';
+import '../styles/header.scss';
+
+import { navSlide } from './other/navSlide';
+
+navSlide();
